e5640e8aedf1557f7fdf1ab5e5cc3596c7f5a91f
amgoh <indzilla101@gmail.com>
2023-12-31 03:07:46 -0500

    Documentation and finalization of webscraper functions

--- tradebot.py
+++ tradebot.py
@@ -5,45 +5,105 @@ from alpaca.trading.requests import GetAssetsRequest
 from alpaca.trading.enums import AssetClass
 from alpaca.trading.requests import MarketOrderRequest
 from alpaca.trading.enums import OrderSide, TimeInForce
+from alpaca.data.requests import StockLatestBarRequest
+from alpaca.data.historical import StockHistoricalDataClient
 import webscraper as web
-
+import random
+import pickle
 
 load_dotenv()
 API_KEY = os.getenv("API-KEY")
 SECRET_KEY = os.getenv("SECRET-KEY")
+
+classifier_file = open("naivebayes.pickle", "rb")
+classifier = pickle.load(classifier_file)
+classifier_file.close()
 
 
 #Initialize trading client and account
 client = TradingClient(API_KEY, SECRET_KEY, paper=True)
 account = client.get_account()
 
-#Check if account is available to trade
-if(account.trading_blocked):
-    print('Account is currently restricted from trading.')
+#Initialize stock data client
+dataClient = StockHistoricalDataClient(API_KEY, SECRET_KEY)
 
 #Calculate current profit/loss value
 balance_change = float(account.equity) - float(account.last_equity)
 
+#Get a list of all stocks
 search_params = GetAssetsRequest(asset_class=AssetClass.US_EQUITY)
-
 assets = client.get_all_assets(search_params)
 
-# for i in assets:
-#     if i.symbol.startswith("Z") and i.tradable:
-#         print(i.symbol)
-        
-# preparing market order
-market_order_data = MarketOrderRequest(
-                    symbol="SPY",
-                    qty=0.023,
+
+def find_stock():
+    """
+    Used to select a random tradable stock from a list of all U.S. Equities
+    
+    Returns: 
+        A dict containing the name, symbol, price, and perceived sentiment related to the stock
+    """
+    asset = random.choice(assets) # choose random stock from assets list
+
+    # make sure selected asset is tradable on Alpaca
+    while(not asset.tradable):
+        asset = random.choice(assets)
+
+    STOCK = asset.name # stock name
+    SYMBOL = asset.symbol # stock symbol
+    
+    # Search for the latest stock pricing information for the random stock
+    REQUEST_PARAMS = StockLatestBarRequest(symbol_or_symbols=SYMBOL)
+    STOCK_BARS = dataClient.get_stock_latest_bar(REQUEST_PARAMS)
+    PRICE = STOCK_BARS.get(SYMBOL).vwap # stock price
+    
+    # Evaluate the current opinion of the stock based on a NaiveBayesClassifier 
+    # and the 10 most popular news articles in the last 24 hours about the stock company
+    SENTIMENT = web.predict_stock_opinion(STOCK, classifier)
+    
+    # Return value, dict
+    stock_info = {
+        'name' : STOCK,
+        'symbol' : SYMBOL,
+        'price' : PRICE,
+        'sentiment' : SENTIMENT
+    }
+    
+    return stock_info
+
+random_stock_info = find_stock()
+stock_name = random_stock_info.get('name')
+stock_symbol = random_stock_info.get('symbol')
+stock_price = random_stock_info.get('price')
+sentiment = random_stock_info.get('sentiment')
+
+print('${} is available as buying power before purchasing {} stock.'.format(account.buying_power, stock_name))
+
+match sentiment:
+    case "neg":
+        try:
+            market_order_data = MarketOrderRequest(
+                        symbol=stock_symbol,
+                        qty=client.get_open_position(stock_symbol).qty,
+                        side=OrderSide.SELL,
+                        time_in_force=TimeInForce.DAY
+            )
+            
+            market_order = client.submit_order(
+                    order_data=market_order_data
+            )
+        except:
+            print("not owned -> find new stock")
+    case "pos":
+        market_order_data = MarketOrderRequest(
+                    symbol=stock_symbol,
+                    qty=1000/stock_price,
                     side=OrderSide.BUY,
                     time_in_force=TimeInForce.DAY
-                    )
-
-# Market order
-market_order = client.submit_order(
+        )
+        
+        market_order = client.submit_order(
                 order_data=market_order_data
-               )
-
-print('${} is available as buying power.'.format(account.buying_power))
+        )
+        
+print('$1000 has been spent on {} shares of {} stock, current buying power is : {}'.format(1000/stock_price, stock_name, account.buying_power))
 print(f'Today\'s portfolio balance change: ${balance_change}')
--- webscraper.py
+++ webscraper.py
@@ -1,14 +1,7 @@
 import requests
-import re
-import urllib3
 from googlesearch import search
 from bs4 import BeautifulSoup
 import nltk
-import pickle
-
-classifier_file = open("naivebayes.pickle", "rb")
-classifier = pickle.load(classifier_file)
-classifier_file.close()
 
 headers = { 
     'User-Agent'      : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36', 
@@ -19,23 +12,38 @@ headers = {
 }
 
 def document_features(document, word_features):
+    """
+    Returns a dict of features, featureset, that can be used to classify a list of words
+    """
     document_words = set(document)
     features = {}
     for word in word_features:
         features['contains({})'.format(word)] = (word in document_words)
     return features
 
+def update_owned_stock_opinion(stock, model):
+    return
+
 def predict_stock_opinion(stock, model):
+    """
+    Returns the sentiment of the specified publicly traded company based on the top 10 news articles from a Google search from the last 2 days
     
-    sentiments = []
+    Parameters:
+        stock - the name of the publicly traded company\n
+        model - a NaiveBayesClassifier used for sentiment analysis of the queried news articles
     
-    for i in search("latest news regarding the {} company".format(stock), tbs="qdr:d", num=10, stop=10, pause=3):
+    Returns:
+        A string, either 'neg' or 'pos' representing the perceived sentiment
+    """
+    sentiments = [] # list of all sentiments from all scraped articles
+    
+    for i in search("latest news regarding the {} company".format(stock), tbs="qdr:2d", num=10, stop=10, pause=3):
         try:
-            print(i)
+            page = requests.get(i, headers=headers, timeout=10) # access webpage, stop if loading exceeds 10 seconds
+            soup = BeautifulSoup(page.content, 'html5lib') # parse HTML
             
-            page = requests.get(i, headers=headers, timeout=10)
-            soup = BeautifulSoup(page.content, 'html5lib')
             
+            # gather all words from the article
             words = ''
             article_tag = soup.find('article')
             if(article_tag != None):
@@ -46,6 +54,7 @@ def predict_stock_opinion(stock, model):
             for i in paragraph_tag:
                 words += " " + i.text
             
+            # create a featureset and classify it using the NaiveBayesClassifier model
             all_words = nltk.FreqDist(w.lower() for w in words.split())
             word_features = list(all_words)[:2000]
             
@@ -54,9 +63,12 @@ def predict_stock_opinion(stock, model):
             
             
         except:
+            # catch exceptions, in case of an error accessing a webpage
             continue
         
+    # check if more negative sentiment
     if(sentiments.count("neg") > sentiments.count("pos")):
         return "neg"
+    # otherwise return positive sentiment, if equal, assume positive
     else:
         return "pos"
